2b4edf6d2b15aba0a8adaa88228eded8160c2af7
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-08 23:12:23 +0300

    rename method

--- service/chat/ichat.go
+++ service/chat/ichat.go
@@ -1,5 +1,10 @@
 package chat
 
+import (
+	"context"
+	"github.com/SyntaxErrorLineNULL/chat-service/domain"
+)
+
 type IChat interface {
-	CreateChat()
+	Create(ctx context.Context, input *domain.Chat) error
 }
