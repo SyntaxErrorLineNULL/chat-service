afd6022a98ab434c99d17dcb2aa4eb5401681aa8
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-08 22:16:40 +0300

    create cannot find error type

--- repository/chat/error.go
+++ repository/chat/error.go
@@ -7,3 +7,6 @@ var ErrEmpty = errors.New("empty")
 
 // ErrNotFound returns when chat doesn't exist
 var ErrNotFound = errors.New("not found")
+
+// ErrCannotFind returns when request was with incorrect data to search user
+var ErrCannotFind = errors.New("cannot find")
