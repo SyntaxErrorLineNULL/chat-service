3c2f34351414efd55427bc213aab37dad88b70e0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-10 23:36:21 +0300

    create base struct

--- repository/chatsusers/chatsuser.go
+++ repository/chatsusers/chatsuser.go
@@ -1,1 +1,20 @@
 package chatsusers
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
+)
+
+type DefaultChatsUsersRepository struct {
+	client *mongo.Client
+	col    *mongo.Collection
+	logger *zap.Logger
+}
+
+func NewDefaultChatsUsersRepository(client *mongo.Client, logger *zap.Logger) *DefaultChatsUsersRepository {
+	return &DefaultChatsUsersRepository{
+		client: client,
+		col:    client.Database("chat-service").Collection("chats_users"),
+		logger: logger,
+	}
+}
