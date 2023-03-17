45b5e9493233a3ec11f48bb5009b7ac53fec48fc
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-17 23:44:13 +0300

    create domain field

--- domain/user.go
+++ domain/user.go
@@ -1,4 +1,14 @@
 package domain
 
 type User struct {
+	// User identifier
+	ID string `json:"id" bson:"id"`
+	// User first name
+	FirstName string `json:"firstname,omitempty" bson:"firstname"`
+	// User last name
+	LastName string `json:"lastname,omitempty" bson:"lastname"`
+	// User name (nickname)
+	UserName string `json:"username" bson:"username"`
+	// User email
+	Email string `json:"email" bson:"email"`
 }
