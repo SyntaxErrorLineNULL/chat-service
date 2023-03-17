b94f0c2e2339e8d5b5a99373c28e948df8549c3f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-18 00:24:29 +0300

    create user method

--- repository/user/user.go
+++ repository/user/user.go
@@ -1,9 +1,13 @@
 package user
 
 import (
+	"context"
 	"errors"
+	"github.com/SyntaxErrorLineNULL/chat-service/domain"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
+	"time"
 )
 
 // ErrEmpty returns when request was with empty data
@@ -18,16 +22,39 @@ var ErrCannotFind = errors.New("cannot find")
 // ErrInternal returns when something went wrong in repository
 var ErrInternal = errors.New("internal error")
 
-type DefaultChatRepository struct {
+// ErrInvalidArgument returns when request was with invalid data
+var ErrInvalidArgument = errors.New("invalid argument")
+
+type DefaultUserRepository struct {
 	client *mongo.Client
 	col    *mongo.Collection
 	logger *zap.Logger
 }
 
-func NewDefaultUserRepository(client *mongo.Client, logger *zap.Logger) *DefaultChatRepository {
-	return &DefaultChatRepository{
+func NewDefaultUserRepository(client *mongo.Client, logger *zap.Logger) *DefaultUserRepository {
+	return &DefaultUserRepository{
 		client: client,
 		col:    client.Database("chat-service").Collection("user"),
 		logger: logger,
 	}
+}
+
+func (r *DefaultUserRepository) Create(ctx context.Context, usr *domain.User) error {
+	l := r.logger.Sugar().With("Create")
+	start := time.Now()
+	if usr == nil {
+		l.Error(zap.Error(ErrEmpty), zap.Duration("duration", time.Since(start)), "user is nil")
+		return ErrInvalidArgument
+	}
+
+	// generate user id
+	usr.ID = uuid.New().String()
+	_, err := r.col.InsertOne(ctx, usr)
+	if err != nil {
+		l.Error(zap.Error(err), zap.Duration("duration", time.Since(start)), "insert error")
+		return ErrInternal
+	}
+
+	l.Info(zap.Duration("duration", time.Since(start)), "successful user created")
+	return nil
 }
