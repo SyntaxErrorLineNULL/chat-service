d76ada39c2461668dcac71ee4f0b5691176e436d
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-19 00:30:12 +0300

    check user exist method

--- service/user/user.go
+++ service/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/SyntaxErrorLineNULL/chat-service/domain"
 	"github.com/SyntaxErrorLineNULL/chat-service/repository"
 	"github.com/SyntaxErrorLineNULL/chat-service/repository/user"
@@ -22,6 +23,9 @@ var ErrUserAlreadyExist = errors.New("user already exist with the same credentia
 
 // ErrIncorrectEmail returns case the email is incorrect
 var ErrIncorrectEmail = errors.New("email is incorrect")
+
+// ErrUserEmptyID returns when userid is empty
+var ErrUserEmptyID = errors.New("empty user id")
 
 // Settings user service setting provider
 type Settings struct {
@@ -45,30 +49,36 @@ func New(settings *Settings) *Service {
 	}
 }
 
+func (srv *Service) methodLogger(ctx context.Context, name string) *zap.Logger {
+	return srv.logger.With(
+		zap.String("layer: {user-service} ", fmt.Sprintf("method: [%s]", name)),
+	)
+}
+
 // Create a new user and save it to the database
 // This function creates a new user by checking if the input data is valid, generating a unique ID, and saving the user to the database.
 // It logs errors and returns error messages if anything goes wrong.
 func (srv *Service) Create(ctx context.Context, input *domain.User) error {
-	// Log the start of the method call
-	l := srv.logger.Sugar().With("Create")
+	// Get the method logger with additional context information.
+	l := srv.methodLogger(ctx, "Create")
 	l.Debug("creating new chat")
 
 	// Check if the input data is empty
 	if input == nil {
-		l.Error(zap.Error(ErrEmpty), "empty request data")
+		l.Error("empty request data", zap.Error(ErrEmpty))
 		return ErrEmpty
 	}
 
 	// check Email validation use net mail pkg
 	if !input.ValidEmail() {
-		l.Error(zap.Error(ErrIncorrectEmail), "failed check email validation")
+		l.Error("failed check email validation", zap.Error(ErrIncorrectEmail))
 		return ErrIncorrectEmail
 	}
 
 	// Check if the user already exists
 	_, err := srv.Find(ctx, input)
 	if err == nil {
-		l.Error(zap.Error(ErrEmpty), "user already exist")
+		l.Error("user already exist", zap.Error(ErrEmpty))
 		return ErrUserAlreadyExist
 	}
 
@@ -78,7 +88,7 @@ func (srv *Service) Create(ctx context.Context, input *domain.User) error {
 
 	err = srv.db.User.Create(ctx, input)
 	if err != nil {
-		l.Error(zap.Error(err), "failed create user")
+		l.Error("failed create user", zap.Error(err))
 		return err
 	}
 
@@ -90,12 +100,13 @@ func (srv *Service) Create(ctx context.Context, input *domain.User) error {
 // Find searches for a user in the database using the provided context and input.
 // Returns the user if found, otherwise returns an error.
 func (srv *Service) Find(ctx context.Context, input *domain.User) (*domain.User, error) {
-	l := srv.logger.Sugar().With("Find")
+	// Get the method logger with additional context information.
+	l := srv.methodLogger(ctx, "Find")
 	l.Debug("find user record")
 
 	// Check if the input is empty
 	if input == nil {
-		l.Error(zap.Error(ErrEmpty), "empty request data")
+		l.Error("empty request data", zap.Error(ErrEmpty))
 		return nil, ErrEmpty
 	}
 
@@ -103,12 +114,12 @@ func (srv *Service) Find(ctx context.Context, input *domain.User) (*domain.User,
 	u, err := srv.db.User.Find(ctx, input)
 	// Check if the user was not found
 	if errors.Is(err, user.ErrNotFound) || errors.Is(err, user.ErrCannotFind) {
-		l.Error(zap.Error(ErrUserNotFound), "user not found")
+		l.Error("user not found", zap.Error(ErrUserNotFound))
 		return input, ErrUserNotFound
 	}
 	// Check if there was an error while finding the user
 	if err != nil {
-		l.Error(zap.Error(err), "failed find user")
+		l.Error("failed find user", zap.Error(err))
 		return input, err
 	}
 
@@ -120,23 +131,49 @@ func (srv *Service) Find(ctx context.Context, input *domain.User) (*domain.User,
 // It takes a context and a user domain object as input,
 // and returns an error if any operation fails.
 func (srv *Service) Update(ctx context.Context, input *domain.User) error {
-	l := srv.logger.Sugar().With("Update")
+	// Get the method logger with additional context information.
+	l := srv.methodLogger(ctx, "Update")
 	l.Debug("update user record")
 
 	// Check if user data is empty
 	if input == nil {
-		l.Error(zap.Error(ErrEmpty), "empty user data")
+		l.Error("empty user data", zap.Error(ErrEmpty))
 		return ErrEmpty
 	}
 
 	// Call the Update method of the User repository to update the user record
 	err := srv.db.User.Update(ctx, input)
 	if err != nil {
-		l.Error(zap.Error(err), "failed update user")
+		l.Error("failed update user", zap.Error(err))
 		return err
 	}
 
 	// Log success and return nil
 	l.Info("successfully update user data")
 	return nil
+}
+
+// Exist checks if a user with the specified ID exists in the database.
+// It returns a boolean indicating whether the user exists or not, along with an error, if any.
+func (srv *Service) Exist(ctx context.Context, userID string) (bool, error) {
+	// Get the method logger with additional context information.
+	l := srv.methodLogger(ctx, "Exist")
+	l.Debug("check user existence")
+
+	// Check if the user ID is empty.
+	if userID == "" {
+		l.Error("empty user data", zap.Error(ErrEmpty))
+		return false, ErrUserEmptyID
+	}
+
+	// Call the database to check if the user exists.
+	exist, err := srv.db.User.Exist(ctx, userID)
+	if err != nil {
+		l.Error("failed check user exist", zap.Error(err))
+		return false, err
+	}
+
+	// Log success and return the result.
+	l.Info("successfully checked user exist")
+	return exist, nil
 }
