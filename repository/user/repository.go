e964f199de117c5165ca3eef97b94844a06ffab0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-18 11:00:22 +0300

    add new method to interface

--- repository/user/repository.go
+++ repository/user/repository.go
@@ -5,8 +5,11 @@ import (
 	"github.com/SyntaxErrorLineNULL/chat-service/domain"
 )
 
-type IUserRepository interface {
+// UserRepository database interface
+type UserRepository interface {
 	Create(ctx context.Context, usr *domain.User) error
 	Find(ctx context.Context, usr *domain.User) (*domain.User, error)
 	Update(ctx context.Context, usr *domain.User) error
+	Exist(ctx context.Context, id string) (bool, error)
+	ExistUserName(ctx context.Context, username string) (bool, error)
 }
