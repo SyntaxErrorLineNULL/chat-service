aca2ebe28e18443a03a989f0c3ca2f3d9b85ade0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2023-03-18 11:00:34 +0300

    base user service

--- service/user/user.go
+++ service/user/user.go
@@ -1,1 +1,20 @@
 package user
+
+import (
+	"github.com/SyntaxErrorLineNULL/chat-service/repository"
+	"go.uber.org/zap"
+)
+
+type Settings struct {
+	DB     *repository.Repository
+	Logger *zap.Logger
+}
+
+type Service struct {
+	db     *repository.Repository
+	logger *zap.Logger
+}
+
+func New(settings *Settings) *Service {
+	return &Service{}
+}
